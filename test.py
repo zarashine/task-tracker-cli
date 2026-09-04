b4ccffbd9af283646a400524d7baaa010fc71275
Zara <zdehdar02@gmail.com>
2026-09-04 06:07:02 +0330

    Implement mark-in-progress command

--- test.py
+++ test.py
@@ -69,7 +69,7 @@ match command:
                     for i, task in enumerate(tasks):
                         if task["id"] == id_num:
                             task["description"] = sys.argv[3]
-                            task["updateیAt"] = time
+                            task["updatedAt"] = time
                             found = True
                             with open("tasks.json", "w") as file:
                                 json.dump(tasks, file)
@@ -104,7 +104,32 @@ match command:
                         print("There is no task with this ID.")
 
     case "mark-in-progress":
-        print("")
+        if len(sys.argv) < 3:
+            print("Task ID and new status are required.")
+        else:
+            try:
+                id_num = int(sys.argv[2])
+            except ValueError:
+                print("Task ID must be number!")
+            else:
+                tasks = load_tasks()
+                time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                found = False
+                if not tasks:
+                    print("No tasks found!")
+                else:
+                    for i, task in enumerate(tasks):
+                        if task["id"] == id_num:
+                            task["status"] = "in-progress"
+                            task["updatedAt"] = time
+                            found = True
+                            with open("tasks.json", "w") as file:
+                                json.dump(tasks, file)
+                            break
+
+                    if not found:
+                        print("There is no task with this ID.")
+
     case "mark-done":
         print("")
     case "list":
